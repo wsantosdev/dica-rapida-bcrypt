284e5a6152a56087b15f3e460ae538f81dc9328c
William Santos <william.santos@msn.com>
2020-08-02 15:57:00 -0300

    Initial commit

---
+++ DicaRapida.Bcrypt.Password/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BCrypt.Net;
+using static BCrypt.Net.BCrypt;
+
+namespace DicaRapida.Bcrypt.Password
+{
+    class Program
+    {
+        private const int WorkFactor = 12;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua nova senha: ");
+            var cleanPassword = Console.ReadLine();
+            var hashedPassword = HashPassword(cleanPassword, WorkFactor);
+            Console.WriteLine($"O hash para sua senha é: {hashedPassword}");
+            
+            Console.WriteLine("Confirme sua nova senha: ");
+            var passwordsMatch = Verify(Console.ReadLine(), hashedPassword);
+            Console.WriteLine($"A verificação foi {(passwordsMatch ? "positiva" : "negativa")}");
+        }
+    }
+}
